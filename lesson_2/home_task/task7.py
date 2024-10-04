9bde9c6edd3392522412f874529dead75ac46c59
NikitaMuksinov <n.muxinoff@gmail.com>
2024-10-04 17:24:53 +0300

    Homework_task7.py

--- lesson_2/home_task/task7.py
+++ lesson_2/home_task/task7.py
@@ -1,2 +1,6 @@
 #todo: Даны три точки A , B , C на числовой оси. Найти длины отрезков AC и BC и их сумму.
 # Примечание: все точки получаем через функцию input().
+a=int(input())
+b=int(input())
+c=int(input())
+print('AC = ',abs(a)+abs(c),'AC = ',abs(b) + abs(c),'summ = ',(abs(a)+abs(c))+(abs(b)+abs(c)))
