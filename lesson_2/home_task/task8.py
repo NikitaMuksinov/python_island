3388671541310c93e508ca61ad70a21b7835a5bb
NikitaMuksinov <n.muxinoff@gmail.com>
2024-10-04 17:37:12 +0300

    Home_work_ task8.py

--- lesson_2/home_task/task8.py
+++ lesson_2/home_task/task8.py
@@ -1,1 +1,3 @@
 # todo: Проверить истинность высказывания: "Данное четырехзначное число читается одинаково слева направо и справа налево".
+number=1223
+print((bool(int(str(abs(number))[0]))==(int(str(abs(number))[3]))) and (int(str(abs(number))[1]))==(int(str(abs(number))[2])))
