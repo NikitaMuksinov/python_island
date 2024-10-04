b3f0f4ad3f0db6acab68eb24ba6db94e437cf6b4
NikitaMuksinov <n.muxinoff@gmail.com>
2024-10-04 16:49:05 +0300

    Homework task5.py

--- lesson_2/home_task/task5.py
+++ lesson_2/home_task/task5.py
@@ -1,4 +1,6 @@
 #todo: Написать программу, которая считывает два числа и выводит их сумму, разность, частное, произведение,
 # результат целочисленного деления, результат деления с остатком, результат возведения в степень.
-
+number1=int(input())
+number2=int(input())
+print(number1+number2,number1-number2,number1*number2,number1/number2,number1//number2,number1%number2,number1**number1)
 
